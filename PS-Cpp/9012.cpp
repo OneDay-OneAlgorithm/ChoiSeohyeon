0e04b3e25a862a2f9125298354a6e98ac0baf30b
candosh <csh200310@naver.com>
2023-06-26 18:28:12 +0900

    boj:9012

---
+++ PS-Cpp/9012.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        stack<char> s;
+        string arr;
+
+        cin >> arr;
+        int len = arr.length();
+
+        bool isValid = true;
+
+        for (int j = 0; j < len; j++) {
+            if (arr[j] == '(') {
+                s.push(arr[j]);
+            }
+            else {
+                if (s.empty()) {
+                    isValid = false;
+                    break;
+                }
+                else {
+                    s.pop();
+                }
+            }
+        }
+        
+        if (!s.empty() || !isValid) {
+            cout << "NO" << "\n";
+        }
+        else {
+            cout << "YES" << "\n";
+        }
+    }
+
+    return 0;
+}
