1b4d8070a387c1dad9ad3812d9eabb003e449783
candosh <csh200310@naver.com>
2023-07-09 22:35:05 +0900

    boj:14581

---
+++ PS-Cpp/14581.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+
+    cin>>name;
+
+    cout<<":fan::fan::fan:"<<"\n";
+    cout<<":fan::"<<name<<"::fan:"<<"\n";
+    cout<<":fan::fan::fan:"<<"\n";
+
+    return 0;
+}
