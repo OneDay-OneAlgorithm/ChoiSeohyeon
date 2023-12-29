9e8ea40fa68153f69cb7a30c4150c106a83ea999
choiseohyeon <csh200310@naver.com>
2023-12-29 17:18:34 +0900

    블랙잭

--- solvedac-class/블랙잭.cpp
+++ solvedac-class/블랙잭.cpp
@@ -19,9 +19,9 @@ int main()
         cin >> arr[i];
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 2; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = i + 1; j < n - 1; j++)
         {
             for (int k = j + 1; k < n; k++)
             {
