2fa7ee6687408b31e0e4170f438c4068db19e3e7
choiseohyeon <csh200310@naver.com>
2024-01-01 18:12:49 +0900

    01/01: 좌표정렬

--- solvedac-class/좌표정렬.cpp
+++ solvedac-class/좌표정렬.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int main()
+{
+    int n, x, y;
+    cin >> n;
+    vector<vector<int>> arr;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        arr.push_back({x, y});
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        // arr[i][0] 벡터의 첫번째 원소
+        // arr[i][1] 벡터의 두번째 원소
+        cout << arr[i][0] << " " << arr[i][1] << '\n';
+    }
+
+    return 0;
+}
