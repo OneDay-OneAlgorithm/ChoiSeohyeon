86d3f651150e1a90c33f58454b9803946fa73b6a
choiseohyeon <csh200310@naver.com>
2024-01-06 17:13:17 +0900

    [add] 1/6: 직사각형

---
+++ PS-boj/17323_직사각형.cpp
@@ -0,0 +1,16 @@
+// 백준 : 직사각형
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 변수 생성
+    int a, b;
+    // 입력
+    cin >> a >> b;
+    // 출력
+    cout << a * b;
+
+    return 0;
+}
