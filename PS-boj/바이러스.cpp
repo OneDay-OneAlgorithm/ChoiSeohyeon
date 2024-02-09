bfe041d1c3eaa155b883ad11407cee663143e605
choiseohyeon <csh200310@naver.com>
2024-02-09 23:44:45 +0900

    [add] 2/9 : 바이러스

--- PS-boj/바이러스.cpp
+++ PS-boj/바이러스.cpp
@@ -1,10 +1,24 @@
 // 실버 3
-// 푸는 중
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int ComNum, Pair;
+vector<int> vals[101];
+int visited[101];
+
+void dfs(int curr)
+{
+    visited[curr] = 1;
+    for (int i = 0; i < vals[curr].size(); ++i)
+    {
+        int next = vals[curr][i];
+        if (visited[next])
+            continue;
+        dfs(next);
+    }
+}
 
 int main()
 {
@@ -12,6 +26,27 @@ int main()
     cin.tie(0);
 
     cin >> ComNum >> Pair;
+
+    for (int i = 0; i < Pair; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        vals[b].push_back(a);
+        vals[a].push_back(b);
+    }
+
+    int res = 0;
+
+    for (int i = 1; i <= ComNum; i++)
+    {
+        if (!visited[i])
+        {
+            dfs(i);
+            res++;
+        }
+    }
+
+    cout << res << "\n";
 
     return 0;
 }
