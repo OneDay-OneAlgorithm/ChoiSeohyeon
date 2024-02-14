6f63acdfeb08138832174a64424788924c7a8844
choiseohyeon <csh200310@naver.com>
2024-02-14 11:15:43 +0900

    [add] 반복수열 코드 보완

--- PS-boj/반복수열.cpp
+++ PS-boj/반복수열.cpp
@@ -1,9 +1,10 @@
+// A: 기준 값, P: 이전 값에서의 각각 곱할 자리수 제곱수
+
 #include <iostream>
 #include <vector>
 #include <cmath>
-#define MAX 100000
-
 using namespace std;
+#define MAX 500000
 
 vector<int> visited(MAX, 0);
 
@@ -11,37 +12,45 @@ int calc(int A, int P)
 {
     int temp = A;
     int sum = 0;
+
     while (temp > 0)
     {
         sum += (int)(pow(temp % 10, P));
         temp /= 10;
     }
+
     return sum;
 }
 
-void dfs(int num, int P)
+void dfs(int val, int P)
 {
-    visited[num]++;
-    if (visited[num] > 2)
+    visited[val]++;
+    if (visited[val] > 2)
+    {
         return;
-    dfs(calc(num, P), P);
+    }
+
+    dfs(calc(val, P), P);
 }
 
 int main()
 {
-    ios::sync_with_stdio();
-    cin.tie();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     int A, P;
     cin >> A >> P;
+
     dfs(A, P);
 
     int count = 0;
 
-    for (int i = 0; i <= MAX; i++)
+    for (int i = 0; i < MAX; i++)
     {
         if (visited[i] == 1)
+        {
             count++;
+        }
         else
             continue;
     }
