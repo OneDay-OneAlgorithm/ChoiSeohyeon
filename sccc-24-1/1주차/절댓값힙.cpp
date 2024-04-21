e8deb4d887ea7553158949b53862e9a2ed48dadf
choiseohyeon <csh200310@naver.com>
2024-04-21 09:35:24 +0900

    [add] 절댓값힙-실버1

---
+++ sccc-24-1/1주차/절댓값힙.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	int x;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		if (x != 0)
+		{
+			pq.push(x);
+		}
+		else if (x == 0)
+		{
+			if (pq.empty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+	}
+	return 0;
+}
