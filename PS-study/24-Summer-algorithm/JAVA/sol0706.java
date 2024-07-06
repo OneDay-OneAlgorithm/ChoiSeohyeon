896550a9434b7d76c4d59ab3c18ef8c4ad02a646
choiseohyeon <csh200310@naver.com>
2024-07-06 22:53:09 +0900

    sol0706

---
+++ PS-study/24-Summer-algorithm/JAVA/sol0706.java
@@ -0,0 +1,12 @@
+public class sol0706 {
+	public int solution(int[] num_list, int n) {
+        int answer = 0;
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]==n){
+                answer=1;
+            }
+        }
+        
+        return answer;
+    }
+}
