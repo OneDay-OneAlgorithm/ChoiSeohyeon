d582d7b04f83cd8edbbddc521928a540cf5b12d9
choiseohyeon <csh200310@naver.com>
2024-07-12 12:33:54 +0900

    sol0712

---
+++ PS-study/24-Summer-algorithm/JAVA/sol0712.java
@@ -0,0 +1,16 @@
+public class sol0712 {
+	public int solution(int price) {
+        int answer = 0;
+        if (price >= 500000) {
+            answer = (int) (price * 0.8);
+        } else if (price >= 300000) {
+            answer = (int) (price * 0.9);
+        } else if (price >= 100000) {
+            answer = (int) (price * 0.95);
+        } else {
+            answer = price;
+        }
+
+        return answer;
+    }
+}
