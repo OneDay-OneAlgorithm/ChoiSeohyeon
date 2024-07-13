0e276553958e3c973e9b40940bf5edda86d11abd
choiseohyeon <csh200310@naver.com>
2024-07-14 00:00:02 +0900

    sol0713

--- PS-study/24-Summer-algorithm/JAVA/sop0703.java
+++ PS-study/24-Summer-algorithm/JAVA/sop0703.java
@@ -1,17 +1,17 @@
 public class sop0703 {
-
-		public int solution(int angle) {
-			int answer = 0;
-			if(angle < 90) {
-				answer = 1;
-			} else if(angle == 90) {
-				answer = 2;
-			} else if(angle < 180) {
-				answer = 3;
-			} else if(angle == 180) {
-				answer = 4;
-			}
-			return answer;
-		}
-
+	public int solution(int angle) {
+        int answer = 0;
+        if(angle==180){
+            answer = 4;
+        }
+        else if(90<angle&& angle<180){
+            answer=3;
+        }
+        else if(90==angle){
+            answer=2;
+        }
+        else answer=1;
+        
+        return answer;
+    }
 }
