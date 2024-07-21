3dc9ec820c64ffa388d6cd9764243942a8af8222
choiseohyeon <csh200310@naver.com>
2024-07-21 16:25:57 +0900

    sol0721

---
+++ PS-study/24-Summer-algorithm/JAVA/sol0721.java
@@ -0,0 +1,11 @@
+public class sol0721 {
+	public int solution(int n) {
+    	int count = 0;
+    	for (int i = 1; i <= n; i++) {
+    		if(n%i==0) {
+    			count++;
+    		}
+		}
+    	return count; 
+    }
+}
